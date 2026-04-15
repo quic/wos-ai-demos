b78c1612219aab795988f9cec88a5df32078779a
Alok Kumar Pandey <quic_alopand@quicinc.com>
2026-04-15 14:19:10 +0530

    Add license

--- demos/DemoApp/src/Models/DexiNedModel.hpp
+++ demos/DemoApp/src/Models/DexiNedModel.hpp
@@ -1,4 +1,9 @@
-
+/*
+**************************************************************************************************
+* Copyright (c) 2026 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+**************************************************************************************************
+*/
 #pragma once
 
 #include <Models/Model.hpp>
