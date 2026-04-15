5cd37d0c97d6afb1a57ba9643edbd071872b71c0
Alok Kumar Pandey <quic_alopand@quicinc.com>
2026-04-15 14:22:34 +0530

    Add license
    Signed-off-by: Alok Kumar Pandey <quic_alopand@quicinc.com>

--- demos/DemoApp/src/Models/MidasV2SmallModel.hpp
+++ demos/DemoApp/src/Models/MidasV2SmallModel.hpp
@@ -4,6 +4,7 @@
 * SPDX-License-Identifier: BSD-3-Clause-Clear
 **************************************************************************************************
 */
+
 #pragma once
 #include <Models/Model.hpp>
 #include <algorithm>
